36bfc24359310b84303f2c048ac168dbf6858072
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-12-11 20:20:21 +0100

    add week 1

--- src/main/kotlin/week1/GenerateParenthesisCombinations.kt
+++ src/main/kotlin/week1/GenerateParenthesisCombinations.kt
@@ -1,4 +1,20 @@
 package week1
 
-fun generateParenthesisCombinations(num: Int): List<String> = TODO()
+fun generateParenthesisCombinations(num: Int): List<String> {
+    val result = mutableListOf<String>()
+    generate(0, 0, num, "", result)
+    return result
+}
 
+fun generate(numOfStart: Int, numOfClose: Int, num: Int, h: String, result: MutableList<String>) {
+    if (numOfStart == numOfClose && numOfClose == num) {
+        result.add(h)
+    } else {
+        if (numOfStart < num) {
+            generate(numOfStart + 1, numOfClose, num, "$h(", result)
+        }
+        if (numOfClose < numOfStart){
+            generate(numOfStart, numOfClose + 1, num, "$h)", result)
+        }
+    }
+}
--- src/main/kotlin/week1/JsonStringify.kt
+++ src/main/kotlin/week1/JsonStringify.kt
@@ -1,5 +1,15 @@
 package week1
 
+import java.text.DecimalFormat
+
+fun stringConcatenation(vararg strings: String): String {
+    val result = StringBuilder()
+    for (s in strings) {
+        result.append(s)
+    }
+    return result.toString()
+}
+
 sealed class JsonElement
 data class JsonObject(val fields: Map<String, JsonElement>) : JsonElement() {
     constructor(vararg fields: Pair<String, JsonElement>) : this(fields.toMap())
@@ -14,5 +24,13 @@ data class JsonString(val value: String) : JsonElement()
 data class JsonBoolean(val value: Boolean) : JsonElement()
 object JsonNull : JsonElement()
 
-fun JsonElement.stringify(): String = TODO()
-
+fun JsonElement.stringify(): String = when (this) {
+    is JsonNull -> "null"
+    is JsonBoolean -> value.toString()
+    is JsonString -> stringConcatenation("\"", value, "\"")
+    is JsonNumber -> DecimalFormat().apply { isDecimalSeparatorAlwaysShown = false }.format(value).toString().replace(",", ".")
+    is JsonArray -> elements.joinToString (",", "[", "]") { it.stringify() }
+    is JsonObject -> fields
+        .map { (k, v) -> stringConcatenation("\"", k, "\":", v.stringify()) }
+        .joinToString(",", "{", "}")
+}
