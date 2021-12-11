ff401405e6befa48bed9cb0f416130a9fc480a59
Wojciech Szlosek <wojciech185sz@gmail.com>
2021-12-11 16:15:13 +0100

    add week 2

--- src/main/kotlin/week2/Tree.kt
+++ src/main/kotlin/week2/Tree.kt
@@ -1,5 +1,6 @@
 package week2
 
+
 sealed class Tree<T> {
     override fun toString(): String = when (this) {
         is Leaf -> value.toString()
@@ -13,4 +14,41 @@ data class Node<T>(val left: Tree<T>, val right: Tree<T>) : Tree<T>()
 fun Tree<*>.count(): Int = when (this) {
     is Leaf -> 1
     is Node -> left.count() + right.count()
+}
+
+fun Tree<*>.countAll(): Int = when (this) {
+    is Leaf -> 1
+    is Node -> 1 + left.countAll() + right.countAll()
+}
+
+fun Tree<*>.height(): Int = when (this) {
+    is Leaf -> 1
+    is Node -> 1 + left.height().coerceAtLeast(right.height())
+}
+
+fun <T : Comparable<T>> Tree<T>.biggest(): T = when (this) {
+    is Leaf -> value
+    is Node -> maxOf(left.biggest(), right.biggest())
+}
+
+fun Tree<*>.sum(): Int = when (this) {
+    is Leaf -> value as Int
+    is Node -> left.sum() + right.sum()
+}
+
+operator fun Tree<*>.contains(s: String?): Boolean = when (this) {
+    is Leaf -> value == s
+    is Node -> left.contains(s) || right.contains(s)
+}
+
+operator fun <T> Tree<T>.plus(second: Tree<T>): Node<T> =
+    Node(this, second)
+
+fun <T> Tree<T>.toList(): List<T> = when (this) {
+    is Leaf -> listOf(value)
+    is Node -> left.toList() + right.toList()
+}
+
+fun main() {
+
 }
